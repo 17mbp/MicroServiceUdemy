b7406e2ede782b872a95a027f2c30123fc9235c8
17mbp <17mvelarde@gmail.com>
2023-04-21 11:11:41 -0600

    inicio exito

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,36 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+        public async Task<ShoppingCart> GetBasket(string UserName)
+        {
+            var basket = await _redisCache.GetStringAsync(UserName);
+            if (String.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        } 
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basketREDIS)
+        {
+            await _redisCache.SetStringAsync(basketREDIS.UserName, JsonConvert.SerializeObject(basketREDIS));
+            return await GetBasket(basketREDIS.UserName);
+        }
+        public async Task DeleteBasket(string UserName)
+        {
+            await _redisCache.RemoveAsync(UserName);
+        }
+    }
+}
