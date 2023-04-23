57d18e56e59c758e5e4aaac6980a662bc98c6278
17mbp <17mvelarde@gmail.com>
2023-04-23 16:08:49 -0600

    grpc voy en consuming discount grpc service from basket

--- src/Services/Discount/Discount.Grpc/Startup.cs
+++ src/Services/Discount/Discount.Grpc/Startup.cs
@@ -1,13 +1,10 @@
 ﻿using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Discount.Grpc
 {
@@ -16,6 +13,7 @@ namespace Discount.Grpc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IDiscountRepository, DiscountRepository>();
+            services.AddAutoMapper(typeof(Startup));
             services.AddGrpc();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -28,7 +26,7 @@ namespace Discount.Grpc
 
             app.UseEndpoints(endpoints =>
             {
-            //    endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<DiscountService>();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
