a137b18ba3c499cb77b9ebd3e5ea4eac8dd7acd7
17mbp <17mvelarde@gmail.com>
2023-09-19 13:45:01 -0600

    FRONTEND WEBAPP consuming services develop in clean architect

---
+++ src/WebApps/AspnetRunBasics/Startup.cs
@@ -0,0 +1,81 @@
+//using AspnetRunBasics.Data;
+//using AspnetRunBasics.Repositories;
+using AspnetRunBasics.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+namespace AspnetRunBasics
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+                
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient<ICatalogService, CatalogService>(c =>
+                c.BaseAddress = new Uri(Configuration["ApiSettings:GatewayAddress"]));
+            services.AddHttpClient<IBasketService, BasketService>(c =>
+                c.BaseAddress = new Uri(Configuration["ApiSettings:GatewayAddress"]));
+            services.AddHttpClient<IOrderService, OrderService>(c =>
+                c.BaseAddress = new Uri(Configuration["ApiSettings:GatewayAddress"]));
+
+
+            #region database services
+            //// use in-memory database
+            //services.AddDbContext<AspnetRunContext>(c =>
+            //    c.UseInMemoryDatabase("AspnetRunConnection"));
+
+            // add database dependecy
+            //services.AddDbContext<AspnetRunContext>(c =>
+            //    c.UseSqlServer(Configuration.GetConnectionString("AspnetRunConnection")));
+
+            #endregion            
+
+            #region project services
+            //// add repository dependecy
+            //services.AddScoped<IProductRepository, ProductRepository>();
+            //services.AddScoped<ICartRepository, CartRepository>();
+            //services.AddScoped<IOrderRepository, OrderRepository>();
+            //services.AddScoped<IContactRepository, ContactRepository>();
+
+            #endregion
+
+            services.AddRazorPages();
+        }
+               
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days.  https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
